392b731acffd58aac0131906f536a4c743d8d24a
DanOlinski <daniel.guterres@hotmail.com>
2023-06-19 16:56:43 -0600

    implement authentication for admin section

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["ADMIN_AUTHENTICATION_NAME"], password: ENV["ADMIN_AUTHENTICATION_PASS"]
+
   def show
   end
 end
