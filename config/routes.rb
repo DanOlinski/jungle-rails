3673e1fa92d3c8d2a895c88b59c3b29627dc7f85
DanOlinski <daniel.guterres@hotmail.com>
2023-06-19 19:52:36 -0600

    implement category display, view and create

--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,18 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-
+  
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
   end
-
+  
   resources :orders, only: [:create, :show]
-
+  
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :create, :new]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
