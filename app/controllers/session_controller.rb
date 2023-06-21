d9d266266ac69df6b496a0dabcea2408737d51b2
DanOlinski <daniel.guterres@hotmail.com>
2023-06-20 19:44:42 -0600

    login user

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,11 +6,12 @@ class SessionController < ApplicationController
     #if pasword and email are correct and user exists save user info as a cookie
     #if authentication doesn't pass redirect to login page
     if user = User.authenticate_with_credentials(params[:email], params[:password])
-      session[:user_id] = user.id
+      session[:user] = user
       redirect_to '/'
     else
       redirect_to '/login'
     end
   end
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+
+  def self.authenticate_with_credentials(email, password)
+    email = email.downcase.strip
+    @user = User.find_by_email(email)
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
 end
